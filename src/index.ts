4bb62b38a432f072c773a430bbfa6276a1c29ad1
Jonas Damtoft <Jomik@users.noreply.github.com>
2026-05-23 20:50:17 +0200

    chore: scaffold project structure

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import type { ExtensionFactory } from "@earendil-works/pi-coding-agent";
+
+const factory: ExtensionFactory = async (_pi) => {
+  // TODO: load config, register file access guards
+};
+
+export default factory;
